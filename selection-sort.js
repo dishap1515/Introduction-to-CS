b2130fdf5a1abf97bf307a9fd4d40caea08a9000
Disha Patel <dishapatelvadodara15@gmail.com>
2020-08-13 00:04:20 +0530

    Sorting numbers using Selection Sort

---
+++ selection-sort.js
@@ -0,0 +1,17 @@
+function selectionSort(array) {
+    
+    for (let i = 0; i < array.length; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] < array[minIndex]) {
+                minIndex = j;
+            }
+        }
+        const temp = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = temp;
+    }
+    return array;
+}
+
+console.log(selectionSort([64,25,12,22,11]));
