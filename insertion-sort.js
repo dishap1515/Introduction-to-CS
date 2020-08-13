3ae0cb6e0c0e96d1a33a17129e9f590f5db15e22
Disha Patel <dishapatelvadodara15@gmail.com>
2020-08-13 18:49:19 +0530

    Sorting numbers using Insertion Sort

---
+++ insertion-sort.js
@@ -0,0 +1,18 @@
+function insertionSort(array) {
+    
+    for (let i = 1; i < array.length; i++) {
+
+        for (let j = i; j > 0; j--) {
+            if (array[j] < array[j-1]) {
+                const temp = array[j];
+                array[j] = array[j-1];
+                array[j-1] = temp;
+            }else {
+                break;
+            }
+        }
+    }
+    return array;
+}
+
+console.log(insertionSort([1, 4, 2, 8, 345, 123, 43, 32, 55, 92]));
