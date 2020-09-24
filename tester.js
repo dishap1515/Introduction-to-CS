60ec9fd8e3538c8d782e57fdf328c0ac96353ac6
Disha Patel <dishap1509@gmail.com>
2020-09-24 16:02:47 +0530

    fix: for loop and error throw

--- tester.js
+++ tester.js
@@ -2,56 +2,39 @@ const ssort = require('./selection-sort');
 const bsort = require('./bubble-sort');
 const isort = require('./insertion-sort');
 
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-function getRandomArray(num) {
-  var ints = [];
-  while (ints.length < num) {
-    var randNum = getRandomInt(1, 91);
-    if(!ints.indexOf(randNum) > -1){
-      ints.push(randNum);
+for (let i = 1; i <= 1000; i++) { 
+    function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
     }
-  }
-  return ints;
-}
-var arr = getRandomArray(10);
+    function getRandomArray(num) {
+    var ints = [];
+    while (ints.length < num) {
+        var randNum = getRandomInt(1, 91);
+        if(!ints.indexOf(randNum) > -1){
+        ints.push(randNum);
+        }
+    }
+    return ints;
+    }
+    var arr = getRandomArray(10);
 
-function sortArray(array) { 
-  for(let i = 1; i < 1000; i++ ) { 
-      array.sort(function(a, b){return a - b});
-  }
-    
-  return array;
-}
+    function sortArray(array) {
+        array.sort(function(a, b){return a - b});
+        return array;
+    }
 
-var selectionSortedArray = ssort.selectionSort(arr.slice());
-var bubbleSortedArray = bsort.bubbleSort(arr.slice());
-var insertionSortedArray = isort.insertionSort(arr.slice());
-var inbuiltSortedArray = sortArray(arr.slice());
+    var selectionSortedArray = ssort.selectionSort(arr.slice());
+    var bubbleSortedArray = bsort.bubbleSort(arr.slice());
+    var insertionSortedArray = isort.insertionSort(arr.slice());
+    var inbuiltSortedArray = sortArray(arr.slice());
 
-if (!inbuiltSortedArray === selectionSortedArray) { 
-  try { 
-    throw ('Array not Sorted.');
-  } catch(e) {
-   
-    console.log(e);
-  }
-}
-if (!inbuiltSortedArray === bubbleSortedArray) { 
-  try { 
-    throw ('Array not Sorted.');
-  } catch(e) {
-   
-    console.log(e);
-  }
-}
-if (!inbuiltSortedArray === insertionSortedArray) { 
-  try { 
-    throw ('Array not Sorted.');
-  } catch(e) {
-   
-    console.log(e);
-  }
+    if (!selectionSortedArray === inbuiltSortedArray) { 
+        throw ('Array not Sorted.');
+    }
+    if (!bubbleSortedArray === inbuiltSortedArray) { 
+        throw ('Array not Sorted.');
+    }
+    if (!insertionSortedArray === inbuiltSortedArray) { 
+        throw ('Array not Sorted.');
+    }
 }
